1cb135eb8b47db54f16a92577cd4d3911ee37bcc
cbooth <callumwbooth@gmail.com>
2014-11-06 21:16:09 +0000

    Slight refactoring

--- XRSLTest/MainWindow.xaml.cs
+++ XRSLTest/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ICSharpCode.AvalonEdit.Highlighting;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
 using System.IO;
+using System.Windows;
 using System.Xml;
 
 namespace XRSLTest
@@ -36,7 +23,7 @@ namespace XRSLTest
                     customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(xr, HighlightingManager.Instance);
                 }
             }
-
+            
             HighlightingManager.Instance.RegisterHighlighting("XRF Shader Sequencing Language", new string[] { ".xssl" }, customHighlighting);
 
             InitializeComponent();
