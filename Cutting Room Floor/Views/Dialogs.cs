089f7d064d084565ea10a987f288440a53c8fa40
cbooth <callumwbooth@gmail.com>
2014-11-15 21:57:55 +0000

    Split dialog definitions and functions into separate view file.

---
+++ Cutting Room Floor/Views/Dialogs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Xrf.Views
+{
+    public static class Dialogs
+    {
+        public static string ShowOpenMovieDialog()
+        {
+            // Self-explanatory, show a filesystem dialog, ask for a movie path.
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Open Movie for editing",
+                CheckFileExists = true,
+                InitialDirectory = Environment.SpecialFolder.MyVideos.ToString()
+            };
+
+            return (openFileDialog.ShowDialog() == true) ? openFileDialog.FileName : null;
+        }
+
+        public static bool ConfirmProjectShutdown()
+        {
+            var intent = MessageBox.Show("Clearing the project will remove all scratchdisks. Any un-exported frames will be lost. Continue?",
+                                             "Cutting Room Floor XRF",
+                                             MessageBoxButton.YesNo,
+                                             MessageBoxImage.Exclamation);
+
+            return (intent == MessageBoxResult.Yes);
+        }
+    }
+}
