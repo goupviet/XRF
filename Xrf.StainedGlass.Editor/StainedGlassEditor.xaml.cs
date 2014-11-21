ed4b7f22583e9e13ae15c117ef3dbe3758eca6b0
Callum Booth <callumwbooth@gmail.com>
2014-11-21 01:25:31 +0000

    Started work on StainedGlassEditor component

--- Xrf.StainedGlass.Editor/StainedGlassEditor.xaml.cs
+++ Xrf.StainedGlass.Editor/StainedGlassEditor.xaml.cs
@@ -18,9 +18,9 @@ namespace Xrf.StainedGlass.Editor
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class StainedGlassEditor : UserControl
     {
-        public UserControl1()
+        public StainedGlassEditor()
         {
             InitializeComponent();
         }
