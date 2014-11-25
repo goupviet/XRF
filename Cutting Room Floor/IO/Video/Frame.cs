a52fa715f1e40ef65d23b657a25948fdfd51182d
cbooth <callumwbooth@gmail.com>
2014-11-25 15:34:49 +0000

    New Frame object, containing the image url, imagesource and frame number

---
+++ Cutting Room Floor/IO/Video/Frame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Xrf.IO.Video
+{
+    public class Frame
+    {
+        public int Index { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Frame(int index, string url)
+        {
+            Index = index;
+            ImageUrl = url;
+        }
+    }
+}
