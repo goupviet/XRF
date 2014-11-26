dadda8cb8c20b5914fd13fc0f079a979ed030bb0
cbooth <callumwbooth@gmail.com>
2014-11-26 02:50:28 +0000

    Added Image Editor viewmodel

---
+++ Cutting Room Floor/IO/Editor/ImageEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Xrf.IO.Editor
+{
+    public class ImageEditor : DependencyObject
+    {
+        public static readonly DependencyProperty OriginalImageProperty = DependencyProperty.Register("OriginalImage", typeof(ImageSource), typeof(ImageEditor));
+        public ImageSource OriginalImage
+        {
+            get { return (ImageSource)GetValue(OriginalImageProperty); }
+            set { SetValue(OriginalImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty EditedImageProperty = DependencyProperty.Register("EditedImage", typeof(ImageSource), typeof(ImageEditor));
+        public ImageSource EditedImage
+        {
+            get { return (ImageSource)GetValue(EditedImageProperty); }
+            set { SetValue(EditedImageProperty, value); }
+        }
+    }
+}
