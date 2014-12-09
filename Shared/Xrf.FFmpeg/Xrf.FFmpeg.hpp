84ebbc3d94a364d641153e8235eeb5024187e3d4
cbooth <callumwbooth@gmail.com>
2014-12-09 18:50:42 +0000

    Refactor: change output function names to match C conventions

--- Shared/Xrf.FFmpeg/Xrf.FFmpeg.hpp
+++ Shared/Xrf.FFmpeg/Xrf.FFmpeg.hpp
@@ -5,9 +5,9 @@ namespace Xrf
 	extern "C"
 	{
 		// Extracts a version of all frames, scaled to the specified scale factor, from the specified video path into the destination directory
-		__declspec(dllexport) int __stdcall ExtractAllFrames(const char *szPath, const char *szDestination, float scaleFactor = 1.0);
+		__declspec(dllexport) int __stdcall extract_all_frames(const char *szPath, const char *szDestination, float scaleFactor = 1.0);
 
 		// Gets the framerate from the specified video file.
-		__declspec(dllexport) double _stdcall GetFrameRate(const char *szPath);
+		__declspec(dllexport) double _stdcall get_frame_rate(const char *szPath);
 	}
 }
